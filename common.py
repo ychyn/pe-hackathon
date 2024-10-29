5f2ff838c7831463b99207f00d53f7ae20746938
Milan François <milan.francois@etu.minesparis.psl.eu>
2024-10-29 17:10:48 +0100

    Milan

--- common.py
+++ common.py
@@ -28,8 +28,39 @@ def calc_indice(tab,poids):
 
 
 
+# +
 # Milan
 Data= pd.read_excel('DataForTable2023.xls')
 NOMS=list(Data.columns)   #les noms des colonnes
 PAYS=Data['Country name'].unique() 
+
+def select(Pays,nom):
+    DYNAMIQUE={}
+    for pays in Pays:
+        mask= Data['Country name']==pays
+        D2= Data[mask]
+        DYNAMIQUE[pays]={}
+        for col in NOMS:
+            if col=='year' or col=='Country name':
+                pass
+            else:
+                a,b=np.polyfit(D2['year'],D2[col], 1)
+                nb=len(D2['year'])
+                DYNAMIQUE[pays][col]=a*nb/17   #17 max du nb d'années pour les données, lisser les données
+    
+    return DYNAMIQUE
+
+np.vectorize(select)
+DYNAMIQUE=select(PAYS,NOMS)
+
+L1=[DYNAMIQUE[pays]['Generosity']+DYNAMIQUE[pays]['Social support']+DYNAMIQUE[pays]['Freedom to make life choices'] for pays in PAYS]
+L2=[DYNAMIQUE[pays]['Log GDP per capita'] for pays in PAYS]
+
+Data2=pd.DataFrame({'Pays':PAYS, 'Dynamique humaine':L1 , 'Dynamique éco':L2})
+Data_hum=Data2.sort_values('Dynamique humaine').reset_index()
+Data_eco=Data2.sort_values('Dynamique éco').reset_index()
+
+Data_hum['Dynamique humaine'].plot(marker='+');
+Data_eco['Dynamique éco'].plot(marker='+');
+plt.legend()
 
